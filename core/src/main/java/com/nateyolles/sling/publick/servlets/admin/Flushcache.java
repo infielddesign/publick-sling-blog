2137b1978980a9f1d10fbd85fb27ab78a47d2885
Nicola Abello <nicola@infielddesign.com>
2015-10-22 14:26:28 -0700

    Added call to invalidate cache when page is edited and saved. This will invalidate the page edited only.

--- core/src/main/java/com/nateyolles/sling/publick/servlets/admin/Flushcache.java
+++ core/src/main/java/com/nateyolles/sling/publick/servlets/admin/Flushcache.java
@@ -12,24 +12,26 @@ import org.slf4j.LoggerFactory;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
 import javax.net.ssl.HttpsURLConnection;
+import javax.servlet.ServletException;
 
 //e.g. - curl -v -X POST -H "CQ-Action: Activate" -H "CQ-Handle: /page/capabilities.html" -H "Content-Length: 0" http://publickdisp.dev/dispatcher/invalidate.cache
 
 @SlingServlet(paths = PublickConstants.SERVLET_PATH_ADMIN + "/flushcache")
 public class Flushcache extends AdminServlet {
 
-    private Logger logger = LoggerFactory.getLogger(this.getClass());
+    private Logger LOGGER = LoggerFactory.getLogger(this.getClass());
 
     // HTTP POST request
     private void invalidate(String url, String handle) throws Exception {
 
         URL obj = new URL(url);
-        HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
 
         //Add request header
         con.setRequestMethod("POST");
@@ -41,8 +43,8 @@ public class Flushcache extends AdminServlet {
         con.setDoOutput(true);
 
         int responseCode = con.getResponseCode();
-        logger.info("Sending 'POST' request to URL : " + url);
-        logger.info("Response Code : " + responseCode);
+        LOGGER.info("Sending 'POST' request to URL : " + url);
+        LOGGER.info("Response Code : " + responseCode);
 
         BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
         String inputLine;
@@ -54,23 +56,24 @@ public class Flushcache extends AdminServlet {
         in.close();
 
         //Print result
-        logger.info("Flushcache response: " + response.toString());
+        LOGGER.info("Flushcache response: " + response.toString());
     }
 
     @Override
-    public void doGet(SlingHttpServletRequest request, SlingHttpServletResponse response) {
+    public void doPost(SlingHttpServletRequest request, SlingHttpServletResponse response)  throws ServletException, IOException {
         try {
-            //retrieve the request parameters
+            //Retrieve the request parameters
             String handle = request.getParameter("handle");
 
-            //hard-coding connection properties is a bad practice, but is done here to simplify the example
-            String server = "publickdisp.dev";
+            //Hard-coding connection properties is a bad practice, but is done here to simplify the example
+            String server = "33.33.33.14";
             String uri = "/dispatcher/invalidate.cache";
             String url = "http://" + server + uri;
             invalidate(url, handle);
+            response.sendRedirect(PublickConstants.ADMIN_PAGE_LIST_PATH + ".html");
 
         } catch (Exception e) {
-            logger.error("Flushcache servlet exception: " + e.getMessage());
+            LOGGER.error("Flushcache servlet exception: " + e.getMessage());
         }
     }
 }
